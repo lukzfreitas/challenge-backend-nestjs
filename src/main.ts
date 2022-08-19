b8b69104c900103a297a8a85ef1766de7e582aab
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-08-19 00:11:45 -0300

    Adding page swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setSwagger(app);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+
+function setSwagger(app) {
+  const config = new DocumentBuilder()
+    .setTitle('Challenge backend Alura')
+    .setDescription('The Challenge backend Alura API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
